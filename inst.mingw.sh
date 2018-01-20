f0a19c6aa209a15597ccf4b2a9fc90e694005168
Ronmi Ren <ronmi.ren@gmail.com>
2018-01-21 05:53:58 +0800

    forget to update package list

--- inst.mingw.sh
+++ inst.mingw.sh
@@ -5,6 +5,7 @@ export GOPATH=/mingw64
 export PATH="$PATH:/mingw64/bin"
 export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
 
+pacman -Ss
 pacman -S --needed --noconfirm mingw-w64-x86_64-go mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gtk2 git mingw-w64-x86_64-toolchain base-devel
 
 go get -v github.com/Ronmi/poe-tool
