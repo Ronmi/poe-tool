7dfe59b16fc119a432441c0dfdcde99bcf6f8e1f
Ronmi Ren <ronmi.ren@gmail.com>
2018-01-21 05:43:34 +0800

    add simple script to build

---
+++ inst.mingw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export GOROOT=/mingw64/lib/go
+export GOPATH=/mingw64
+export PATH="$PATH:/mingw64/bin"
+export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
+
+pacman -S --needed --noconfirm mingw-w64-x86_64-go mingw-w64-x86_64-gtk3 mingw-w64-x86_64-gtk2 git mingw-w64-x86_64-toolchain base-devel
+
+go get -v github.com/Ronmi/poe-tool
